7320081e59e5be07bc83f732fe7d1b9425fa1fc5
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-11-15 18:48:46 -0800

    just copying config.js -> config.live.js and keeping api url (/v2)

--- config.live.js
+++ config.live.js
@@ -1,6 +1,13 @@
-if(!config) config = {};
-Object.merge( config, {
+//
+// some desktop-oriented app config overrides
+//
+
+if(typeof(config) == 'undefined') config = {};
+Object.merge(config, {
 	api_url: 'https://api.turtl.it/v2',
+	client: 'desktop',
+	cookie_login: false,
+	base_url: window.location.toString().replace(/^(.*)\/.*?$/, '$1/app'),
 	console: true
 });
 
