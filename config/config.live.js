dfd03190c47bbeb298cb9051cbff9e6d161ffc90
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-09-22 17:14:30 -0700

    config: remove cookie_login (dep)

--- config/config.live.js
+++ config/config.live.js
@@ -9,7 +9,6 @@ if(typeof(config) == 'undefined') var config = {};
 		// don't change this without also changing lib/node/preload/ipc_core.js
 		client: 'desktop',
 		version: app_version,
-		cookie_login: true,
 		base_url: typeof(window) != 'undefined' ? window.location.toString().replace(/^(.*)\/.*?$/, '$1/app') : '',
 		dispatch_port: 7471,
 		core: {
